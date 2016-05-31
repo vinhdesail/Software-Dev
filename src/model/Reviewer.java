b64c986e15e491a39e3622462b29723e8e3af2dd
Edie Campbell <meganelc@uw.edu>
2016-05-30 17:30:25 -0700

    Reviewer Tests complete

--- src/model/Reviewer.java
+++ src/model/Reviewer.java
@@ -6,58 +6,57 @@ import java.util.List;
 
 /**
  * Reviewer class description.
- * @author
+ * @author Edie Megan Campbell
  * @author 
  * @version 2016.05.08
  */
 public class Reviewer extends Role implements Serializable {
 	
-	/** A Reviewer may be assigned no more than 4 papers to Review. */
-	public static final int MAX_PAPERS = 4;
+	/** The maximum number of Manuscripts a Reviewer can be assigned to Review. */
+	public static final int MAX_MANUSCRIPTS = 4;
 	
-	private List<Manuscript> myPapers;
+	private List<Manuscript> myManuscripts;
 
-	/** Generated serialization number. */
 	private static final long serialVersionUID = -3658253011793370271L;
 	
 	/**
-	 * Constructor for Reviewer
-	 * @param myUser - the unique username of the User who is becoming a Reviewer
-	 * @param myConference - the Conference for which this User will be acting as a Reviewer
+	 * @param theUser - the unique username of the User who is becoming a Reviewer
+	 * @param theConference - the Conference for which this User will be acting as a Reviewer
 	 */
-	public Reviewer(String myUser, Conference myConference) {
-		super("Reviewer", myUser, myConference);
-		myPapers = new ArrayList<>();
+	public Reviewer(String theUser, Conference theConference) {
+		super("Reviewer", theUser, theConference);
+		myManuscripts = new ArrayList<>();
 	}
 	
 	/**
 	 * TEST ONLY
 	 * Overloaded constructor for making particular instances of Reviewers in unit tests.
-	 * @param myUser - the unique username of the User who is becoming a Reviewer
-	 * @param myConference - the Conference for which this User will be acting as a Reviewer
-	 * @param thePapers - the list of Papers assigned to this Reviewer
+	 * @param theUser - the unique username of the User who is becoming a Reviewer
+	 * @param theConference - the Conference for which this User will be acting as a Reviewer
+	 * @param theManuscripts - the list of Papers assigned to this Reviewer
 	 */
-	public Reviewer(String myUser, Conference myConference, List<Manuscript> thePapers) {
-		super("Reviewer", myUser, myConference);
-		myPapers = new ArrayList<>(thePapers);
+	public Reviewer(String theUser, Conference theConference, List<Manuscript> theManuscripts) {
+		super("Reviewer", theUser, theConference);
+		myManuscripts = new ArrayList<>(theManuscripts);
 	}
 
 	/**
-	 * Submits a Review by this Reviewer for the given Manuscript
+	 * Submits a Review by this Reviewer and specified by the URL of the Review contents for 
+	 * the given Manuscript.
 	 * @param theManuscript
-	 * @param theReviewText
+	 * @param theReviewURL
 	 * @throws IllegalArgumentException if this Reviewer has not been assigned to theManuscript 
 	 * yet or if this Reviewer has already submitted a review for theManuscript
 	 */
-	public void submitReview(Manuscript theManuscript, String theReviewText) {
-		if (!myPapers.contains(theManuscript)) {
+	public void submitReview(Manuscript theManuscript, String theReviewURL) {
+		if (!myManuscripts.contains(theManuscript)) {
 			throw new IllegalArgumentException(this.getMyUsername() + " has not been assigned "
 					+ "to review the Manuscript: " + theManuscript.getTitle());
 		} else if (getMyReview(theManuscript) != null) {
 			throw new IllegalArgumentException(this.getMyUsername() + "has already submitted a review for the"
 					+ " Manuscript: " + theManuscript.getTitle());	
 		}
-		Review review = new Review(this.getMyUsername(), theManuscript.getTitle(), theReviewText);
+		Review review = new Review(this.getMyUsername(), theManuscript.getTitle(), theReviewURL);
 		theManuscript.addReview(review);
 	}
 	
@@ -70,7 +69,7 @@ public class Reviewer extends Role implements Serializable {
 	 * if this Reviewer has not yet submitted a review for this Manuscript to edit.
 	 */
 	public void editReview(Manuscript theManuscript, String theReviewText) {
-		if (!myPapers.contains(theManuscript)) {
+		if (!myManuscripts.contains(theManuscript)) {
 			throw new IllegalArgumentException(this.getMyUsername() + " has not been assigned "
 					+ "to review the Manuscript: " + theManuscript.getTitle());
 		}
@@ -88,8 +87,8 @@ public class Reviewer extends Role implements Serializable {
 	 * Accessor method 
 	 * @return returns the ArrayList of all papers assigned to this Reviewer
 	 */
-	public List<Manuscript> viewMyPapers() {
-		return myPapers;
+	public List<Manuscript> getMyManuscripts() {
+		return myManuscripts;
 	}
 	
 	/**
@@ -99,22 +98,23 @@ public class Reviewer extends Role implements Serializable {
 	 * @throws IllegalArgumentException if this Reviewer already has the max allowed number of papers.
 	 */
 	public void assignReview(Manuscript theManuscript) {
-		if (myPapers.size() < MAX_PAPERS) {
-			myPapers.add(theManuscript);
+		if (myManuscripts.size() < MAX_MANUSCRIPTS) {
+			myManuscripts.add(theManuscript);
 		} else {
-			throw new IllegalArgumentException("Can't assign more than " + MAX_PAPERS + 
+			throw new IllegalArgumentException("Can't assign more than " + MAX_MANUSCRIPTS + 
 					" Manuscripts to this reviewer!");
 		}	
 	}
 	
 	/**
 	 * Searches through a given Manuscript's Reviews so that this Reviewer can access their own.
-	 * @param theManuscript
-	 * @return the Review object associated with this reviewer's unique username, or null if there is none
+	 * @param theManuscript - the Manuscript Object the review was written about
+	 * @return the Review object associated with this reviewer's unique username, or null if 
+	 * there is none
 	 * @throws IllegalArgumentException if this Reviewer has not been assigned to review theManuscript
 	 */
 	public Review getMyReview(Manuscript theManuscript) throws IllegalArgumentException {
-		if (!myPapers.contains(theManuscript)) {
+		if (!myManuscripts.contains(theManuscript)) {
 			throw new IllegalArgumentException(this.getMyUsername() + " has not been assigned "
 					+ "to review the Manuscript: " + theManuscript.getTitle());
 		}
@@ -127,17 +127,23 @@ public class Reviewer extends Role implements Serializable {
 		return null;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public boolean equals(Object theOther) {
 		if (!(theOther instanceof Reviewer && super.equals(theOther))) {
 			return false;
 		}
 		Reviewer theReviewer = (Reviewer) theOther;
-		return myPapers.equals(theReviewer.myPapers);
+		return myManuscripts.equals(theReviewer.myManuscripts);
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int hashCode() {
-		return super.hashCode() + myPapers.hashCode();
+		return super.hashCode() + myManuscripts.hashCode();
 	}
 }
